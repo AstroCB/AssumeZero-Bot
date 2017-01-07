fd838c5b19f45bd05c9dd3e0e6ebb585410b7662
Cameron Bernhardt <cambernhardt@me.com>
2017-01-07 04:38:54 -0500

    Ping app to keep awake

--- heroku.js
+++ heroku.js
@@ -1,4 +1,12 @@
-var express = require('express');
-var app = express();
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+const express = require("express");
+const http = require("http");
+const app = express();
+
+// Bind to ports to make Heroku happy even though it doesn't use them
+app.set("port", (process.env.PORT || 3000));
+app.listen(app.get("port"));
+
+// Ping every 45 minutes to keep awake
+setInterval(function() {
+    http.get("http://assume-bot.herokuapp.com");
+}, 2700000);
