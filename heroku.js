1ebfd6413fbe316b01cf9467d136e2da0d5b5f9e
Cameron Bernhardt <cambernhardt@me.com>
2017-01-07 04:46:16 -0500

    Add time restrictions to ping

--- heroku.js
+++ heroku.js
@@ -7,6 +7,13 @@ app.set("port", (process.env.PORT || 3000));
 app.listen(app.get("port"));
 
 // Ping every 45 minutes to keep awake
+// Sleep from 3 AM to 9 AM to preserve time (UTC)
+const local_low = 3;
+const local_high = 9
+const offset = 5;
 setInterval(function() {
-    http.get("http://assume-bot.herokuapp.com");
+    var now = new Date();
+    if (now.getHours() < (local_low + offset) || now.getHours() >= (local_high + offset)) {
+        http.get("http://assume-bot.herokuapp.com");
+    }
 }, 2700000);
