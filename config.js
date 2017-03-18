f65bebe9e44c3542dda5e21f2f8e1a4f3912f3d8
Cameron Bernhardt <cambernhardt@me.com>
2017-03-18 14:27:05 -0400

    Update commands

--- config.js
+++ config.js
@@ -98,7 +98,7 @@ exports.addBufferLimit = 5;
 // List of responses for "answer" command
 exports.answerResponses = ["Yes", "No", "Maybe", "It is certain", "Time will tell", "Ask again later",
     "Only if you believe", "Possibly", "Never", "Of course", "No way", "You're out of your mind", "Nope",
-    "Yep", "Good one", "Eh"
+    "Yep", "Good one", "Eh", "Si crees", "Por supuesto", "...", "Welp"
 ];
 
 // Default bounds for RNG command
