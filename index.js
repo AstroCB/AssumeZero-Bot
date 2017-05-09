92f213d51011acae7c857300a39397c96f52daac
Cameron Bernhardt <cambernhardt@me.com>
2017-05-09 01:31:35 -0400

    Update commands

--- index.js
+++ index.js
@@ -912,7 +912,7 @@ function handleCommand(command, fromUserId, groupInfo, messageLiteral, api = gap
         processImage(url, attachments, groupInfo, (img, filename) => {
             if (gauss) {
                 // Gaussian blur (extremely resource-intensive – will pretty much halt the bot while processing)
-                sendMessage("This could take awhile...", threadId, () => {
+                sendMessage("Hang on, this might take me a bit...", threadId, () => {
                     const now = (new Date()).getTime();
                     img.gaussian(pixels).write(filename, (err) => {
                         if (!err) {
