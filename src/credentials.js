17dacb0f806a8ec4458b9767e95de1d9ed3cade3
lenhattruong165 <46161374+lenhattruong165@users.noreply.github.com>
2019-09-15 14:36:04 +0700

    Create credentials.js

---
+++ src/credentials.js
@@ -0,0 +1,31 @@
+exports.EMAIL = "xus36399@bcaoo.com";
+
+exports.PASSWORD = "Nhattruong";
+
+// Heroku application token
+
+exports.TOKEN = "";
+
+// MemCachier credentials (from MemCachier dashboard)
+
+exports.MEMCACHIER_PASSWORD = "96C7D101AC4BEA49A596476F9B76E75E";
+
+exports.MEMCACHIER_SERVERS = "mc5.dev.ec2.memcachier.com";
+
+exports.MEMCACHIER_USERNAME = "0CD9F8";
+
+// Spotify API credentials
+
+exports.SPOTIFY_CLIENTID = "";
+
+exports.SPOTIFY_CLIENTSECRET = "";
+
+// Wolfram short-answer API key
+
+exports.WOLFRAM_KEY = "";
+
+// Open Weather Map API key
+
+exports.WEATHER_KEY = "";
+
+© 2019 GitHub, Inc.
