2f2fbe86cfa6134126ef84c5029cecec5ae54ea2
Cameron Bernhardt <cambernhardt@me.com>
2020-07-24 15:50:35 -0400

    Fix deploy script for non-reinstalls

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # Pulls latest changes and restarts the bot
 git checkout -- package-lock.json # Ignore local package-lock changes
 git pull origin master
-if [ $1 == "--reinstall" ]
+if [ ! -z "$1" ] && [ $1 == "--reinstall" ]
 then
     npm install
     npm update
