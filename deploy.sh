7ffda857fce53f8c3cf2bb48336d342a314614fa
Cameron Bernhardt <cambernhardt@me.com>
2021-02-15 00:14:16 -0500

    Tweak deploy script to ignore local package changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # Pulls latest changes and restarts the bot
-git checkout -- package-lock.json # Ignore local package-lock changes
+git checkout -- package*.json # Ignore local package changes
 git pull origin master
 if [ ! -z "$1" ] && [ $1 == "--reinstall" ]
 then
